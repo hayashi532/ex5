8a881ec8d046df9f71c4869499f16bb650b47dda
ryo <hayahayashiryo@gmail.com>
2023-01-11 17:33:47 +0900

    strlen

--- main.c
+++ main.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    printf("hello");
-    return 0;
+ char a[4];
+ int n;
+
+ a[0] = 'a';
+ a[1] = 'b';
+ a[2] = 'c';
+ a[3] = '\0';
+
+ printf("%s\n", a);
+
+ n = strlen(a);
+
+ printf("%d\n", n);
+
+ return 0;
 }
